9243f56f67d03fa4c51b288046c63aeb281a1645
omerbastug <bomersami@gmail.com>
2022-11-26 14:31:47 +0100

    s3 bucket connection

--- rest/__init__.py
+++ rest/__init__.py
@@ -3,17 +3,23 @@ from generateMeme import *
 from decouple import config
 from sqlalchemy.sql.expression import select, func
 from flask_sqlalchemy import SQLAlchemy
- 
-import os
+import boto3
+
 user = config("DB_USER")
 password = config("DB_PASSWORD")
 host = config("DB_HOST")
 port = config("DB_PORT")
 database = config("DB_NAME")
+s3AccesID = config("S3_ACCES_KEY_ID")
+s3AccessSecret = config("S3_ACCESS_SECRET")
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://'+user+':'+password+'@'+host+':'+port+'/'+database
 db = SQLAlchemy(app)
+
+s3client = boto3.client("s3",
+    aws_access_key_id=s3AccesID,
+    aws_secret_access_key=s3AccessSecret)
 
 from rest.Models import User,Category,TemplateCategory,Meme, MemeText
 
