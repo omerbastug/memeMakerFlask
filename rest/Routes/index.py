17c8b27dc7e67390f67fe4afa162353633727b44
omerbastug <bomersami@gmail.com>
2022-11-27 07:27:57 +0100

    jinja templates

--- rest/Routes/index.py
+++ rest/Routes/index.py
@@ -4,4 +4,5 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html",imgsrc="/api/meme/random")
+    return render_template("homepage.html",imgsrc="/api/meme/random", LoggedIn= False)
+    # return render_template("index.html",imgsrc="/api/meme/random")
