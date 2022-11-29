74d745f0be05d921bfafb75d0c9baca5ee7c4f97
omerbastug <bomersami@gmail.com>
2022-11-29 12:05:56 +0100

    registering with flask_login

--- flaskapp/Routes/registerPage.py
+++ flaskapp/Routes/registerPage.py
@@ -1,7 +1,22 @@
 from flaskapp import app
-from generateMeme import *
-from flask import render_template
+from flask import render_template, request, redirect, flash, url_for
+from flaskapp.Models.User import User
+from flask_login import login_user
 
-@app.route("/register")
+@app.route("/register", methods=['GET'])
 def register_meme_page():
     return render_template("registerPage.html")
+
+
+@app.route("/register", methods=['POST'])
+def register_redirect():
+    body = request.form
+    try:
+        newuser = User(fullName=body.get("fullname"),email=body.get("email"),password = body.get("password"))
+        newuser.addToDB()
+        login_user(newuser)
+        flash("Account created.", category="success")
+        return redirect(url_for("index"))
+    except BaseException as err:
+        flash("oopsies", category='danger')
+        return redirect(url_for("register_meme_page"))
